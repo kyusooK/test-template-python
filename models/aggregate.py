d1987bbba97edd1d6e409331008e14339c406774
kyusoo <rbtn110@uengine.org>
2025-04-08 16:49:09 +0900

    c

--- models/aggregate.py
+++ models/aggregate.py
@@ -20,10 +20,18 @@ class {{namePascalCase}}(Base):
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
     
     def __repr__(self):
-        return f"<{{namePascalCase}}({{#aggregateRoot.fieldDescriptors}}{{#if isKey}}{{nameCamelCase}}={self.{{nameCamelCase}}}{{/if}}{{/aggregateRoot.fieldDescriptors}})>"
+        return f"<{{namePascalCase}}({{#aggregateRoot.fieldDescriptors}}{{#if isKey}}{{nameCamelCase}}={{#createLeftMustache}}{{/createLeftMustache}}self.{{nameCamelCase}}{{#createRightMustache}}{{/createRightMustache}}{{/if}}{{/aggregateRoot.fieldDescriptors}})>"
 
 <function>
 window.$HandleBars.registerHelper('isLong', function (className) {
     return (className === 'Long' || className === 'Integer' || className === 'int');
+});
+window.$HandleBars.registerHelper('createLeftMustache', function () {
+    // Handlebars에서 f-string의 중괄호를 이스케이프하기 위한 헬퍼 함수
+    return '{'
+});
+window.$HandleBars.registerHelper('createRightMustache', function () {
+    // Handlebars에서 f-string의 중괄호를 이스케이프하기 위한 헬퍼 함수
+    return '{'
 });
 </function> 
